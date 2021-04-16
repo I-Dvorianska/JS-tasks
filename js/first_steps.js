c2e444d62ced395de722e8a583e53aa4ccab483f
Ivanna Dvorianska <idvoryanska@gmail.com>
2021-04-16 19:52:47 +0200

    new tasks

--- js/first_steps.js
+++ js/first_steps.js
@@ -1,43 +1,80 @@
-function getSubscriptionPrice(type) {
-  let price;
-  // Пиши код ниже этой строки
+'use strict';
+// function getSubscriptionPrice(type) {
+//   let price;
+ 
+//   // Пиши код ниже этой строки
 
- switch (type) { // Дополни эту строку
-    case 'starter' // Дополни эту строку
-      :price = 0; // porque se pone dos puntos antes de price
-      break;
+//  switch (type) { // Дополни эту строку
+//     case 'starter' // Дополни эту строку
+//       :price = 0; // porque se pone dos puntos antes de price
+//       break;
 
-    case 'professional': // Дополни эту строку
-      price = 20; // Дополни эту строку
-      break;
+//     case 'professional': // Дополни эту строку
+//       price = 20; // Дополни эту строку
+//       break;
 
-    case 'organization': // Дополни эту строку
-      price = 50; // Дополни эту строку
-      break;
-  }
+//     case 'organization': // Дополни эту строку
+//       price = 50; // Дополни эту строку
+//       break;
+//   }
 
-  // Пиши код выше этой строки
-  return price;
-}
-
-
+//   // Пиши код выше этой строки
+//   return price;
+// }
 
 
-function getShippingCost(country) {
-  let message;
-  // Пиши код ниже этой строки
-switch (country) {
-  case Китай:
-    message = `Доставка в ${Китай} будет стоить ${100} кредитов`;
-    case Чили:
-    message = `Доставка в ${Чили} будет стоить ${250} кредитов`;
-    case Австралия:
-    message = `Доставка в ${Австралия} будет стоить ${170} кредитов`;
-    case Ямайка:
-    message = `Доставка в ${Ямайка} будет стоить ${120} кредитов`;
-  default:
-    message = 'Извините, в вашу страну доставки нет';
-}
-  // Пиши код выше этой строки
-  return message;
-}
+
+
+// function getShippingCost(country) {
+//   let message;
+//   // Пиши код ниже этой строки
+//   switch (country) {
+//     case 'Китай':
+//       message = `Доставка в ${'Китай'} будет стоить ${100} кредитов`;
+//     case 'Чили':
+//       message = `Доставка в ${'Чили'} будет стоить ${250} кредитов`;
+//     case 'Австралия':
+//       message = `Доставка в ${'Австралия'} будет стоить ${170} кредитов`;
+//     case 'Ямайка':
+//       message = `Доставка в ${'Ямайка'} будет стоить ${120} кредитов`;
+//     default:
+//       message = 'Извините, в вашу страну доставки нет';
+//   }
+//   // Пиши код выше этой строки
+//   return message;
+// }
+
+
+// function getSubstring(string, length) {
+//   const substring = string.slice(0, [length]); // Дополни эту строку
+
+//   return substring;
+// }
+
+// SLICE
+
+// function formatMessage(message, maxLength) {
+//   let result;
+
+// if (message.length <= maxLength) {
+// result = message;
+// } else {
+// result = message.slice(0, [maxLength]) + '...';
+// }
+//   return result;
+// }
+
+
+// function checkForSpam(message) {
+//   let result;
+//   // Пиши код ниже этой строки
+//  let messagenormalize = message.toLowerCase();
+//  if (messagenormalize.includes('spam') || messagenormalize.includes('sale')) {
+//   result = true; 
+//  } else {
+//  result = false;
+//  }
+//   // Пиши код выше этой строки
+//   return result;
+// }
+
