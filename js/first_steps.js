56e8ef4e356e3b19bc6e873f1bcb54ffbc9e1884
Ivanna Dvorianska <idvoryanska@gmail.com>
2021-04-23 20:22:17 +0200

    finish of module 2

--- js/first_steps.js
+++ js/first_steps.js
@@ -426,3 +426,47 @@
 //     return newArray;
 // }
 // console.log(getEvenNumbers(1, 10));
+
+// 30
+
+// const start = 6;
+// const end = 27;
+// let number;
+
+// for (let i = start; i < end; i += 1) {
+//   if (i % 5 === 0) {
+//     number = i;
+//     break;
+//   }
+// }
+
+// 31
+
+// function findNumber(start, end, divisor) {
+//   // Пиши код ниже этой строки
+ 
+
+//   for (let i = start; i < end; i += 1) {
+//     if (i % divisor === 0) {
+     
+//       return i;
+//     }
+//   }
+
+
+//   // Пиши код выше этой строки
+// }
+
+// 32
+
+// function includes(array, value) {
+//   // Пиши код ниже этой строки
+// for (let i = 0; i < array.length; i +=1) {
+//  if (array[i] === value) {
+//    return true;
+//  }
+// }
+// return false;
+ 
+//   // Пиши код выше этой строки
+// }
