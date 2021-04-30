f7af4f664f2a27c71345e2c36c24ae5ae1621bd8
Ivanna Dvorianska <idvoryanska@gmail.com>
2021-04-30 20:52:01 +0200

    tasks antil 30

--- js/first_steps.js
+++ js/first_steps.js
@@ -537,7 +537,7 @@
 //     price: 2153,
 //     tags: ['premium', 'promoted', 'top'],
 //   };
-  
+
 //   // Пиши код ниже этой строки
 //   const aptRating = apartment.rating;
 //   const aptDescr = apartment.descr;
@@ -649,9 +649,348 @@
 // const credentials = {
 // [emailInputName]: 'henry.carter@aptmail.com',
 //    [passwordInputName]: 'jqueryismyjam',
-  
+
 //   // Пиши код выше этой строки
 // };
 
-10
+// 10
 
+// const apartment = {
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Пиши код ниже этой строки
+// for (key in apartment) {
+// keys.push(key);
+//   values.push(apartment[key]);
+// }
+
+// 11
+
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: 'apt',
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = 'Просторная квартира в центре';
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Пиши код ниже этой строки
+// if (apartment.hasOwnProperty(key)){
+//   keys.push(key);
+//   values.push(apartment[key]);}
+
+//   // Пиши код выше этой строки
+// }
+
+// 12
+
+// function countProps(object) {
+//   let propCount = 0;
+ 
+//     for (const key in object) {
+//         if (object.hasOwnProperty([key]) === false) {
+//             
+//         } else {
+//             propCount = Object.keys(object).length;
+//         }  
+//     }
+    
+//   return propCount;
+// }
+
+// console.log(countProps({ name: 'Mango', age: 2 }));
+
+// 13
+
+// const apartment = {
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Пиши код ниже этой строки
+// const keys = Object.keys(apartment);
+
+// for (const key of keys) {
+    
+    
+//     values.push(apartment[key]);
+    
+// }
+// console.log(values);
+
+// 14
+
+// function countProps(object) {
+//   // Пиши код ниже этой строки
+//   let propCount = 0;
+
+//   for (const key of Object.keys(object)) {
+    
+//       propCount += 1;
+    
+//   }
+
+//   return propCount;
+//   // Пиши код выше этой строки
+// }
+
+// 15
+
+// const apartment = {
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+// };
+// // Пиши код ниже этой строки
+// const keys = Object.keys(apartment);
+// const values = Object.values(apartment);
+
+// 16
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Пиши код ниже этой строки
+//     for (const key of Object.keys(salaries)) {
+        
+//         totalSalary += salaries[key];
+// }
+//   // Пиши код выше этой строки
+//   return totalSalary;
+// }
+
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+
+// 17
+
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Пиши код ниже этой строки
+// for (const color of colors) {
+//     hexColors.push(color.hex);
+//     rgbColors.push(color.rgb);
+// }
+// console.log(hexColors);
+// console.log(rgbColors);
+
+// 18
+
+// const products = [
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//   { name: 'Сканер', price: 2700, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//     for (const product of products) {
+         
+//         if (product.name === productName) {
+           
+//             return product.price;
+//         } 
+//     }
+// return null;
+// }
+
+// console.log(getProductPrice('Радар'));
+// console.log(getProductPrice('Захват'));
+
+// 19
+
+// const products = [
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//   { name: 'Сканер', price: 2700, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//     const valuesArray = [];
+
+//     for (const product of products) {
+//         const keys = Object.keys(product);
+       
+//         for (const key of keys) {
+//             console.log(key);
+//             if (key === propName) {
+//                 valuesArray.push(product[key]);
+//            }
+//         }
+//     }
+//     return valuesArray;
+// }
+
+// console.log(getAllPropValues('quantity'));
+
+// 20
+
+// const products = [
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//    { name: 'Сканер', price: 2700, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//     let sum = 0;
+
+//     for (const product of products) {
+//         console.log(product.name);
+//         if (product.name === productName) {
+//              sum = product.price * product.quantity;
+//     }
+//     }
+//     return sum;
+// }
+// console.log(calculateTotalPrice('Дроид'));
+
+// 21
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Пиши код ниже этой строки
+// const { yesterday, tomorrow, today } = highTemperatures;
+
+
+// // Пиши код выше этой строки
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// 22
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+
+// // Пиши код ниже этой строки
+// const { yesterday, tomorrow, today, icon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg' } = highTemperatures;
+
+
+// // Пиши код выше этой строки
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// 23
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Пиши код ниже этой строки
+// const { yesterday: highYesterday, today: highToday, tomorrow: highTomorrow, icon: highIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg' } = highTemperatures;
+
+
+// // Пиши код выше этой строки
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+// 24
+
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Пиши код ниже этой строки
+
+// for (const { hex, rgb } of colors) {
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
+
+// 25
+
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Пиши код ниже этой строки
+// const { today: { low: lowToday, high: highToday, icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg' },
+//     tomorrow: { low: lowTomorrow, high: highTomorrow,
+//         icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg' } }  = forecast;
+
+// 26
+
+// // Пиши код ниже этой строки
+// function calculateMeanTemperature(forecast) {
+//   const { today: { low: todayLow, high: todayHigh }, tomorrow: { low: tomorrowLow, high: tomorrowHigh}} = forecast;
+  
+
+//   // Пиши код выше этой строки
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+
+// 27
+
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+// // Пиши код ниже этой строки
+// const bestScore = (Math.max(...scores));
+// const worstScore = (Math.min(...scores))
+
+// 28
+
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Пиши код ниже этой строки
+// const allScores = [...firstGroupScores,...secondGroupScores,...thirdGroupScores];
+// const bestScore = Math.max(...allScores);
+// const worstScore = Math.min(...allScores);
+
+// 29
+
+// const defaultSettings = {
+//   theme: 'light',
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+// // Пиши код ниже этой строки
+// const finalSettings = {...defaultSettings, ...overrideSettings};
+
+// 30
+
+// function makeTask(data) {
+//   const completed = false;
+//   const category = 'Общее';
+//   const priority = 'Обычный';
+
+//     const newOb = {category,priority,completed,...data};
+
+//     return newOb;
+// }
