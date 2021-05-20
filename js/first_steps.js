022362dc42f22b4008963508bd5c5c9065c1dba6
Ivanna Dvorianska <idvoryanska@gmail.com>
2021-05-20 21:29:17 +0200

    24 tasks module 6

--- js/first_steps.js
+++ js/first_steps.js
@@ -2150,5 +2150,131 @@
 // }
 // console.log(getUsersWithEyeColor(users , 'brown'));
 
-19
+// 19
+
+const friendsList = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+    age: 37
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+    age: 34
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+    age: 24
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+    age: 21
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+    age: 27
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+    age: 38
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+    age: 39
+  }
+]
+
+// const getUsersWithAge = (users, minAge, maxAge) => {
+//     return users.filter(user => user.age < maxAge && user.age > minAge
+//     );
+   
+// };
+// console.log(getUsersWithAge(friendsList, 20, 30));
+
+// 20
+
+// const getUsersWithFriend = (users, friendName) => {
+//    return users.filter(user =>  user.friends.includes(friendName)
+//     )
+// };
+// console.log(getUsersWithFriend(friendsList, 'Briana Decker'));
+
+// 21
+
+// const getFriends = (users) => {
+//     const friends = users.flatMap(user => user.friends);
+    
+//     return friends.filter((friend, index, self) => self.indexOf(friend) === index )  
+// };
+// console.log(getFriends(friendsList));
+
+// 22
+
+// const getActiveUsers = (users) => {
+//  return users.filter(user => user.isActive === true) 
+// };
+
+// 23
+
+// const getInactiveUsers = (users) => {
+//   return users.filter(user => user.isActive === false) 
+// };
+
+// 24
+
+// const books = [
+//   { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+//   { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+//   { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+//   { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+// ];
+// const BOOK_TITLE = 'Сон смешного человека';
+// const AUTHOR = 'Роберт Шекли';
+// // Пиши код ниже этой строки
+
+// const bookWithTitle = books.find(book => book.title === BOOK_TITLE);
+// const bookByAuthor = books.find(book => book.author === AUTHOR);
+
+25
 
